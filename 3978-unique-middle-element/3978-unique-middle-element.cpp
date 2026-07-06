a2c59bdab716587304e1f672ea1838d8559b51b0
shaswatshukla0234 <shaswats271@gmail.com>
2026-07-06 14:33:37 +0530

    Time: 0 ms (100%), Space: 31.1 MB (57.14%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.cpp
+++ 3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isMiddleElementUnique(vector<int>& nums) {
         int mid = (nums.size())/2;
-        cout << mid ;
         for(int i = 0 ; i < nums.size() ; i++ )
         {
             if(i != mid && nums[i] == nums[mid])    return false;
